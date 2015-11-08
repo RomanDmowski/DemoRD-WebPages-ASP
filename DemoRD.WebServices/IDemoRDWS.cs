e245c9182476698730572786477662459f0f3bf9
Roman Dmowski <roman.dmowski@outlook.com>
2015-11-08 18:42:53 -0500

    New layer - project added DemoRD.WebServices

---
+++ DemoRD.WebServices/IDemoRDWS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DemoRD.WebServices
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface IDemoRDWS
+    {
+        [OperationContract]
+        List<WS_user> GetListUsers();
+    }
+}
---
+++ DemoRD.WebServices/WS_user.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DemoRD.DTO;
+
+namespace DemoRD.WebServices
+{
+    public class WS_user
+    {
+        public Guid User_ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; } 
+
+        private static WS_user convertUserToWS_user (User _user)
+        {
+            WS_user _wsUser = new WS_user();
+
+            _wsUser.FirstName = _user.FirstName;
+            _wsUser.LastName = _user.LastName;
+            _wsUser.Login = _user.Login;
+            _wsUser.User_ID = _user.User_ID;
+
+            return _wsUser;
+        }
+
+
+        public static List<WS_user> ConvertListUserToListWS_user (List<User> _listUser)
+        {
+
+            List<WS_user> _wsList = new List<WS_user>();
+
+            foreach (User _user in _listUser)
+            {
+                _wsList.Add(convertUserToWS_user(_user));
+            }
+
+            return _wsList;
+
+
+        }
+    }
+}
