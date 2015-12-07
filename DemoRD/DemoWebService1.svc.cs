62ca3236cd9f6f4718bf540e7937d8d3acfb5d64
Roman Dmowski <roman.dmowski@outlook.com>
2015-12-06 22:53:42 -0500

    Before Entity Framework test

--- DemoRD/DemoWebService1.svc.cs
+++ DemoRD/DemoWebService1.svc.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
-using System.Text;
 
 using DemoRD.DTO;
 using DemoRD.Domain;
